29f86a5c595f7e36262a05cd1ae0789941784717
hughstonrogers1 <hughstonrogers1>
2021-03-31 19:48:20 -0400

    Static_Exercise

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TempConverter.FahrenheitToCelcius(32));
+
+            Console.WriteLine(TempConverter.CelciusToFahrenheit(0));
+
+            Console.WriteLine(TempConverter.CelciusToFahrenheit(100));
+
+            Console.WriteLine(TempConverter.FahrenheitToCelcius(212));
+        }
+    }
+}
---
+++ TempConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    static class TempConverter
+    {
+        public static double F = 0;
+        public static double C = 0;
+        public static double FahrenheitToCelcius(double fTemp)
+        {
+            C = (fTemp - 32) / 1.8000;
+            return C;
+        }
+        public static double CelciusToFahrenheit(double cTemp)
+        {
+            
+            F = (cTemp * (9 / 5)) + 32;
+                return F;
+        }
+    }
+}
